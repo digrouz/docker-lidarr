2381458bdf8ad9646b764774b9e2390575d445f9
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2021-04-19 15:56:11 +0200

    prepare for github actions

---
+++ .github/scripts/update.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+LIDARR_URL="https://api.github.com/repos/Lidarr/Lidarr/tags"
+
+FULL_LAST_VERSION=$(curl -SsL ${LIDARR_URL} | jq .[0].name -r )
+LAST_VERSION="${FULL_LAST_VERSION:1}"
+
+sed -i -e "s|LIDARR_VERSION='.*'|LIDARR_VERSION='${LAST_VERSION}'|" Dockerfile*
+
+if output=$(git status --porcelain) && [ -z "$output" ]; then
+  # Working directory clean
+  echo "No new version available!"
+else 
+  # Uncommitted changes
+  git commit -a -m "update to version: ${LAST_VERSION}"
+  git push
+fi
