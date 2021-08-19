dc8858ba931731eb3d96c23703a802a377273397
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2021-08-19 09:51:23 +0000

    update to version: 0.8.0.2041

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -3,7 +3,7 @@
 LIDARR_URL="https://api.github.com/repos/Lidarr/Lidarr/releases"
 
 FULL_LAST_VERSION=$(curl -SsL ${LIDARR_URL} | jq .[2].name -r )
-LAST_VERSION="${FULL_LAST_VERSION:1}"
+LAST_VERSION="${FULL_LAST_VERSION}"
 
 sed -i -e "s|LIDARR_VERSION='.*'|LIDARR_VERSION='${LAST_VERSION}'|" Dockerfile*
 
